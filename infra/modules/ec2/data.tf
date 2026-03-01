ec8ea52217fd37d7caaddc3fd7bb8434da14e7fb
l-furquim <furquimmsw@gmail.com>
2026-03-01 11:38:30 -0300

    fix: AMI data filter

--- infra/modules/ec2/data.tf
+++ infra/modules/ec2/data.tf
@@ -1,11 +1,14 @@
 data "aws_ami" "ubuntu_ami" {
   most_recent = true
 
-  virtualization_type = "hvm"
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 
   # Owner oficial da Cannonical (distribuidora do ubuntu)
